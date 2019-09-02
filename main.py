b9086de681ab783309ab3ef6315b6e32fa74244b
j-bd <recodata.out@gmail.com>
2019-09-02 15:24:13 +0200

    Readme_v3

--- main.py
+++ main.py
@@ -5,34 +5,18 @@ Created on Thu Aug 15 14:17:16 2019
 
 @author: j-bd
 
-Implementing Mask RCNN for our own dataset:
-Step 1: Clone Mask RCNN repository
-Step 2: As mentionned by authors, we need first to create a sub-class config
-based on mrcnn/config.py. It will be not automatically implemented here
-Step 3: We create the structure of the project. We need:
-    A master project folder (PROJECT_DIR),
-    A sub-folder 'train' with all training images (automatically recogmised),
-    A sub-folder 'val' with all validation images (automatically recogmised),
-    A sub-folder 'test' with all test images,
-    A sub-folder that will gather training logs during proceding (BACKUP).
-Step 4: We need a pre-trained weights file. If we choose Coco or Imagenet, we can
-download it with provided commands (thanks !). We can also used a last file (for
-instance, if we want to resume a training)
-Step 5: We organised images between "train" and "val" (validation) folders. We
-offer the possibility to choose the amount of data while respecting the ratio
-between images with and without mask
-Step 6: We charge the configuration file create in step 2
-Step 7: We instantiate the MaskRCNN Class present in mrcnn/model.py
-Step 8: We load the pre-trained weights file. See step 4
-Step 9: We instantiate the ShipDataset class (based on Dataset class in
-mrcnn/utils.py) for:
-    managing training data
-    managing validation data
-Step 10: Training can be launch
+To launch the training process:
+python main.py -c train -of "/path/to/kaggle/data/folder/"
+-pf "/path/to/your/project/folder/" -w "/path/to/your/file.h5"
+All these arguments are mandatory. Regarding the weights choice "-w". You either
+can provide:
+    the path to a file "wights.h5"
+    or enter "coco" or "imagenet" to make automaticaly downloading
 
-Two parameters can be modulated for the training :
-    Numbers of steps per epoch in ship_config.py
-    Numbers of epochs in ship_functions.py
+To lauch detection:
+python main.py -c detection -of "/path/to/kaggle/data/folder/"
+ -pf "/path/to/your/project/folder/" -w "/path/to/your/file.h5"
+All these arguments are mandatory.
 """
 
 import ship_functions
